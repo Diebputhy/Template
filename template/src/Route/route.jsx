332353a61800aa13053d61c5ea88020769301e2f
Dieb puthy <puthydieb@gmail.com>
2024-08-29 14:02:06 +0700

    Routes fixed

--- template/src/Route/route.jsx
+++ template/src/Route/route.jsx
@@ -1,9 +1,11 @@
+import { App } from 'antd'
 import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
 
-function Route() {
-  return (
-    <div>Route</div>
-  )
-}
-
-export default Route
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: App()
+  }
+]);
+export {router};
